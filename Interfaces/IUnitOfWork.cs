4c0023e46f17739612ee7e23b24be6d70a2f2fa5
lcsmota <sacul_m@protonmail.com>
2022-12-15 22:08:45 -0300

    Add unit of work

---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+namespace AccountApi.Interfaces;
+
+public interface IUnitOfWork
+{
+    IOwnerRepository OwnerRepository { get; }
+    IAccountRepository AccountRepository { get; }
+    Task Commit();
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+using AccountApi.Context;
+using AccountApi.Interfaces;
+
+namespace AccountApi.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IOwnerRepository _ownerRep;
+    private IAccountRepository _accountRep;
+
+    private readonly AppDbContext _context;
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IOwnerRepository OwnerRepository
+        => _ownerRep ??= new OwnerRepository(_context);
+
+    public IAccountRepository AccountRepository
+        => _accountRep ??= new AccountRepository(_context);
+
+
+    public Task Commit()
+        => _context.SaveChangesAsync();
+
+    public void Dispose()
+        => _context.Dispose();
+}
