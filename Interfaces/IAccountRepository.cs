6d0511ca8a803b834902baa3cd7fb30faf518851
lcsmota <sacul_m@protonmail.com>
2022-12-18 20:50:29 -0300

    Add Account repository interface

--- Interfaces/IAccountRepository.cs
+++ Interfaces/IAccountRepository.cs
@@ -4,5 +4,11 @@ namespace AccountApi.Interfaces;
 
 public interface IAccountRepository : IGenericRepository<Account>
 {
+    Task<IEnumerable<Account>> GetAccountsAsync();
+    Task<Account> GetAccountByIdAsync(Guid id);
+    Task<IEnumerable<Account>> GetAccountsByOwner(Guid ownerId);
 
+    void InsertAccount(Account account);
+    void UpdateAccount(Account account);
+    void DeleteAccount(Account account);
 }
