2d75287e118b7eafff9c3c642d3e1893b88a1e04
lcsmota <sacul_m@protonmail.com>
2022-12-18 20:50:08 -0300

    Add prop OwnerId

--- DTOs/AccountDTO.cs
+++ DTOs/AccountDTO.cs
@@ -5,4 +5,5 @@ public class AccountDTO
     public Guid AccountId { get; set; }
     public DateTime DateCreated { get; set; }
     public string? AccountType { get; set; }
+    public Guid OwnerId { get; set; }
 }
