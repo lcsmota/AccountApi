729024458ace1b5a109cc6b2eb721a00b0ffb140
lcsmota <sacul_m@protonmail.com>
2022-12-18 20:50:51 -0300

    Add Account repository

--- Repository/AccountRepository.cs
+++ Repository/AccountRepository.cs
@@ -1,6 +1,7 @@
 using AccountApi.Context;
 using AccountApi.Interfaces;
 using AccountApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AccountApi.Repository;
 
@@ -8,4 +9,37 @@ public class AccountRepository : GenericRepository<Account>, IAccountRepository
 {
     public AccountRepository(AppDbContext context)
         : base(context) { }
+
+
+    public async Task<Account> GetAccountByIdAsync(Guid id)
+        =>
+            await GetByCondition(e => e.AccountId.Equals(id))
+            .FirstOrDefaultAsync();
+
+
+    public async Task<IEnumerable<Account>> GetAccountsAsync()
+        =>
+            await GetAll()
+            .OrderBy(e => e.DateCreated)
+            .ToListAsync();
+
+
+    public async Task<IEnumerable<Account>> GetAccountsByOwner(Guid ownerId)
+        =>
+            await GetByCondition(e => e.OwnerId.Equals(ownerId))
+            .Include(e => e.Owner)
+            .ToListAsync();
+
+
+    public void InsertAccount(Account account)
+        => Insert(account);
+
+
+    public void UpdateAccount(Account account)
+        => Update(account);
+
+
+    public void DeleteAccount(Account account)
+        => Delete(account);
+
 }
