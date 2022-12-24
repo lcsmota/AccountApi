c451d789b8ac3887f73d9c00d3e3a1b73be570f4
lcsmota <sacul_m@protonmail.com>
2022-12-24 12:04:29 -0300

    Add new props to the filter

--- Pagination/OwnersParameters.cs
+++ Pagination/OwnersParameters.cs
@@ -2,5 +2,8 @@ namespace AccountApi.Pagination;
 
 public class OwnersParameters : QueryStringParameters
 {
+    public uint MinYearOfBirth { get; set; }
+    public uint MaxYearOfBirth { get; set; } = (uint)DateTime.UtcNow.Year;
 
+    public bool ValidYearRange => MaxYearOfBirth > MinYearOfBirth;
 }
