fd6d0ff780fa36cb6d39cbe93023f6ea44d50c2d
lcsmota <sacul_m@protonmail.com>
2022-12-26 22:21:45 -0300

    Add props for filtering and searching

--- Pagination/AccountsParameters.cs
+++ Pagination/AccountsParameters.cs
@@ -2,5 +2,10 @@ namespace AccountApi.Pagination;
 
 public class AccountsParameters : QueryStringParameters
 {
+    public uint MinDateCreated { get; set; }
+    public uint MaxDateCreated { get; set; } = (uint)DateTime.Now.Year;
 
+    public bool ValidDateRange => MinDateCreated < MaxDateCreated;
+
+    public string AccountType { get; set; }
 }
