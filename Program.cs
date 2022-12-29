e7ab2592ec1de075e5b418d39c5f2e0739e33467
lcsmota <sacul_m@protonmail.com>
2022-12-29 11:34:27 -0300

    Add authentication and swagger security

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using System.Text;
 using System.Text.Json.Serialization;
 using AccountApi.Context;
 using AccountApi.DTOs.Mappings;
 using AccountApi.Interfaces;
 using AccountApi.Repository;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +19,23 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                     .AddEntityFrameworkStores<AppDbContext>()
                     .AddDefaultTokenProviders();
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+
+                ValidIssuer = builder.Configuration.GetSection("TokenConf:Iss").Value,
+                ValidAudience = builder.Configuration.GetSection("TokenConf:Aud").Value,
+
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    builder.Configuration.GetSection("Jwt:Key").Value
+                ))
+            });
 
     builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -30,6 +50,31 @@ var builder = WebApplication.CreateBuilder(args);
             Title = "Account API",
             Version = "v1",
             Description = "Simple CRUD using Entity Framework 7"
+        });
+
+        sw.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Header de autorização JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer'[espaço] e o seu token.\r\n\r\nEx.:'Bearer 3refadif234adfea'"
+        });
+
+        sw.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[]{}
+            }
         });
     });
 
@@ -59,6 +104,8 @@ var app = builder.Build();
     app.UseCors("Origins");
 
     app.UseHttpsRedirection();
+
+    app.UseAuthentication();
 
     app.UseAuthorization();
 
